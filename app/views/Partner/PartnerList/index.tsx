ff6f3ba9cb9a0de70505f4c9c4bbf375450409d1
crssstha <shresthacyrus85@gmail.com>
2026-01-13 14:16:03 +0545

    feat(partner): Added partner listing and edit add form

---
+++ app/views/Partner/PartnerList/index.tsx
@@ -0,0 +1,109 @@
+import React, {
+    useCallback,
+    useMemo,
+} from 'react';
+import { useNavigate } from 'react-router';
+import {
+    Button,
+    Container,
+    Pager,
+    Table,
+} from '@ifrc-go/ui';
+import {
+    createElementColumn,
+    createNumberColumn,
+    createStringColumn,
+} from '@ifrc-go/ui/utils';
+
+import TableActions, { TableActionsProps } from '#components/TableAction';
+import {
+    PartnerQuery,
+    useDeletePartnerMutation,
+    usePartnerQuery,
+} from '#generated/types/graphql';
+import usePagination from '#hooks/usePagination';
+
+import styles from './styles.module.css';
+
+type PartnerListItem = NonNullable<PartnerQuery['partners']>['results'][number];
+
+function PartnerList() {
+    const navigate = useNavigate();
+    const {
+        page,
+        setPage,
+        pageSize,
+        variables,
+        getFormattedData,
+    } = usePagination();
+
+    const [{ fetching, data }, reExecuteQuery] = usePartnerQuery({ variables });
+    const [{ fetching: deletePending }, deletePartner] = useDeletePartnerMutation();
+
+    const tableData = useMemo(
+        () => getFormattedData<PartnerListItem>(data?.partners.results),
+        [data, getFormattedData],
+    );
+
+    const handleDelete = useCallback(
+        (id: string, closeModal: () => void) => {
+            deletePartner({ id }).then((resp) => {
+                if (resp.data?.deletePartner) {
+                    reExecuteQuery();
+                    closeModal();
+                }
+            });
+        },
+        [deletePartner, reExecuteQuery],
+    );
+
+    const columns = useMemo(() => [
+        createNumberColumn<PartnerListItem & { sn: number }, string | number>('sn', 'S.N.', (item) => item.sn, { columnWidth: 60 }),
+        createStringColumn<PartnerListItem, string | number>('title', 'Tile', (dept) => dept.title),
+        createStringColumn<PartnerListItem, string | number>('scope', 'Scope', (dept) => dept?.scope),
+        createElementColumn<PartnerListItem, string | number, TableActionsProps>(
+            'actions',
+            'Actions',
+            TableActions,
+            (_, datum) => ({
+                id: datum.id,
+                handleConfirmButtonChange: handleDelete,
+                confirmPending: deletePending,
+                itemTitle: datum.title,
+            }),
+            { columnWidth: 150 },
+        ),
+    ], [handleDelete, deletePending]);
+    return (
+        <Container
+            className={styles.partner}
+            childrenContainerClassName={styles.content}
+            heading="Partner"
+            actions={(
+                <Button name={undefined} variant="primary" disabled={false} onClick={() => navigate('add')}>
+                    Add Partner
+                </Button>
+            )}
+            footerActions={(
+                <Pager
+                    activePage={page}
+                    itemsCount={data?.partners.totalCount ?? 0}
+                    maxItemsPerPage={pageSize}
+                    onActivePageChange={setPage}
+                />
+            )}
+        >
+            <Table
+                keySelector={(item) => item.id}
+                className={styles.table}
+                columns={columns}
+                data={tableData}
+                filtered={false}
+                pending={fetching}
+                headerRowClassName={styles.headerRow}
+            />
+        </Container>
+    );
+}
+
+export default PartnerList;
---
+++ app/views/Partner/query.ts
@@ -0,0 +1,92 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { gql } from 'urql';
+
+const PARTNER_QUERY = gql`
+    query Partner($pagination: OffsetPaginationInput) {
+        partners(pagination: $pagination) {
+            pageInfo {
+                limit
+                offset
+            }
+            totalCount
+            results {
+                id
+                image {
+                    name
+                    size
+                    url
+                }
+                modifiedBy {
+                    firstName
+                    lastName
+                }
+                createdBy {
+                    lastName
+                    firstName
+                }
+                scope
+                title
+            }
+        }
+    }
+`;
+
+const PARTNER_DETAIL = gql`
+    query PartnerDetail($id: ID!) {
+        partner(id: $id) {
+            title
+            scope
+            image {
+                name
+                size
+                url
+            }
+            modifiedBy {
+                lastName
+                firstName
+            }
+            id
+            createdBy {
+                firstName
+                lastName
+            }
+        }
+    }
+`;
+const CREATE_PARTNER_MUTATION = gql`
+    mutation CreatePartner($data: PartnerCreateInput!) {
+        createPartner(data: $data) {
+            ... on PartnerTypeMutationResponseType {
+                errors
+                ok
+            }
+        }
+    }
+`;
+
+const UPDATE_PARTNER_MUTATION = gql`
+    mutation UpdatePartner($pk: ID!, $data: PartnerUpdateInput!) {
+        updatePartner(pk: $pk, data: $data) {
+            ... on PartnerTypeMutationResponseType {
+                errors
+                ok
+            }
+        }
+    }
+`;
+
+const DELETE_PARTNER = gql`
+    mutation DeletePartner($id: ID!) {
+        deletePartner(data: { id: $id }) {
+            ... on OperationInfo {
+                __typename
+                messages {
+                    message
+                }
+            }
+            ... on PartnerType {
+                id
+            }
+        }
+    }
+`;
