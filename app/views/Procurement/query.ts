a5cae34d28ff3564ba68978e24e33db65046dd58
crssstha <shresthacyrus85@gmail.com>
2026-01-14 11:25:37 +0545

    feat(procurement): Added procurement listing and edit add form

---
+++ app/views/Procurement/query.ts
@@ -0,0 +1,83 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { gql } from 'urql';
+
+const PROCUREMENT_QUERY = gql`
+    query Procurement($pagination: OffsetPaginationInput) {
+        procurements(pagination: $pagination) {
+            pageInfo {
+                limit
+                offset
+            }
+            totalCount
+            results {
+                title
+                publishedDate
+                id
+                expiryDate
+            }
+        }
+    }
+`;
+
+const PROCUREMENT_DETAIL_QUERY = gql`
+    query ProcurementDetail($id: ID!) {
+        procurement(id: $id) {
+            title
+            publishedDate
+            modifiedBy {
+                firstName
+                lastName
+            }
+            id
+            file {
+                name
+                size
+                url
+            }
+            expiryDate
+            description
+            createdBy {
+                firstName
+                lastName
+            }
+        }
+    }
+`;
+
+const CREATE_PROCUREMENT_MUTATION = gql`
+    mutation CreateProcurement($data: ProcurementCreateInput!) {
+        createProcurement(data: $data) {
+            ... on ProcurementTypeMutationResponseType {
+                errors
+                ok
+            }
+        }
+    }
+`;
+
+const UPDATE_PROCUREMENT_MUTATION = gql`
+    mutation UpdateProcurement($pk: ID!, $data: ProcurementUpdateInput!) {
+        updateProcurement(pk: $pk, data: $data) {
+            ... on ProcurementTypeMutationResponseType {
+                errors
+                ok
+            }
+        }
+    }
+`;
+
+const DELETE_PROCUREMENT = gql`
+    mutation DeleteProcurement($id: ID!) {
+        deleteProcurement(data: { id: $id }) {
+            ... on ProcurementType {
+                title
+            }
+            ... on OperationInfo {
+                __typename
+                messages {
+                    message
+                }
+            }
+        }
+    }
+`;
