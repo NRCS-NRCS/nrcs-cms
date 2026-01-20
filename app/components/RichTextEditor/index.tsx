cd1e2ee625feccc56fd9ee2b96208881b74cf8bb
crssstha <shresthacyrus85@gmail.com>
2026-01-20 12:09:09 +0545

    feat(editor): added mdx-editor for rich text

--- app/components/RichTextEditor/index.tsx
+++ app/components/RichTextEditor/index.tsx
@@ -1,7 +1,29 @@
-import { Heading } from '@ifrc-go/ui';
-import { Editor } from '@tinymce/tinymce-react';
-import DOMPurify from 'dompurify';
-import parse from 'html-react-parser';
+import '@mdxeditor/editor/style.css';
+
+import {
+    useEffect,
+    useMemo,
+    useRef,
+} from 'react';
+import {
+    BlockTypeSelect,
+    BoldItalicUnderlineToggles,
+    CreateLink,
+    headingsPlugin,
+    imagePlugin,
+    InsertImage,
+    linkDialogPlugin,
+    linkPlugin,
+    listsPlugin,
+    ListsToggle,
+    markdownShortcutPlugin,
+    MDXEditor,
+    MDXEditorMethods,
+    quotePlugin,
+    thematicBreakPlugin,
+    toolbarPlugin,
+    UndoRedo,
+} from '@mdxeditor/editor';
 
 import styles from './styles.module.css';
 
@@ -10,45 +32,75 @@ interface Props {
     onChange?: (value: string) => void;
 }
 
-const API_KEY = import.meta.env.APP_TINYMCE_API_KEY;
+function debounce<Args extends unknown[], R>(
+    fn: (...args: Args) => R,
+    delay: number,
+): (...args: Args) => void {
+    let timeoutId: ReturnType<typeof setTimeout>;
 
-export default function RichTextEditor({ value, onChange }: Props) {
-    const editorOptions = {
-        height: 600,
-        menubar: false,
-        statusbar: false,
-        paste_data_images: false,
-        plugins: 'advlist autolink code help link lists fullscreen',
-        toolbar:
-            'bold italic subscript superscript link | blocks '
-            + 'alignleft aligncenter alignright alignjustify | '
-            + 'bullist numlist outdent indent | fullscreen | help',
-        contextmenu: 'link',
-        block_formats:
-            'Paragraph=p; Heading 1=h1; Heading 2=h2; Heading 3=h3; Heading 4=h4;',
-        branding: false,
-        advlist_bullet_styles: 'default',
-        advlist_number_styles: 'default',
-        content_style: 'body { font-size: 14px}',
+    return (...args: Args) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            fn(...args);
+        }, delay);
     };
+}
+
+function ToolbarContents() {
+    return (
+        <>
+            <UndoRedo />
+            <BoldItalicUnderlineToggles />
+            <BlockTypeSelect />
+            <ListsToggle />
+            <CreateLink />
+            <InsertImage />
+        </>
+    );
+}
+
+export default function RichTextEditor({ value = '', onChange }: Props) {
+    const ref = useRef<MDXEditorMethods>(null);
+    const prevValueRef = useRef(value);
+
+    const handleChange = useMemo(
+        () => debounce((data: string) => {
+            onChange?.(data);
+        }, 300),
+        [onChange],
+    );
+
+    useEffect(() => {
+        if (ref.current && value !== prevValueRef.current) {
+            ref.current.setMarkdown(value);
+            prevValueRef.current = value;
+        }
+    }, [value]);
+
+    const plugins = useMemo(() => [
+        headingsPlugin(),
+        listsPlugin({ enableOrdered: true, enableUnordered: true }),
+        imagePlugin(),
+        linkPlugin(),
+        linkDialogPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        markdownShortcutPlugin(),
+        toolbarPlugin({
+            toolbarClassName: 'my-classname',
+            toolbarContents: ToolbarContents,
+        }),
+    ], []);
 
     return (
-        <div className={styles.editorContainer}>
-            <Editor
-                apiKey={API_KEY}
-                value={value}
-                onEditorChange={(content) => {
-                    onChange?.(content);
-                }}
-                init={editorOptions}
-
+        <div className={styles.editor}>
+            <MDXEditor
+                markdown={value}
+                ref={ref}
+                onChange={handleChange}
+                placeholder="Start writing here..."
+                plugins={plugins}
             />
-            <div className={styles.preview}>
-                <Heading level={2} className={styles.previewHeader}>Preview</Heading>
-                <div className={styles.previewContent}>
-                    {parse(DOMPurify.sanitize(value ?? ''))}
-                </div>
-            </div>
         </div>
     );
 }
