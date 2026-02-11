c2b1621636ad3394d95cba5d726bebf34a13d7b8
crssstha <shresthacyrus85@gmail.com>
2026-02-11 13:51:42 +0545

    feat(web-app-serve): added web app serve

--- env.ts
+++ env.ts
@@ -1,14 +1,27 @@
 import {
-    defineConfig,
     Schema,
-} from '@julr/vite-plugin-validate-env';
+    defineConfig,
+    overrideDefineForWebAppServe,
+} from '@togglecorp/vite-plugin-validate-env';
+
+const webAppServeEnabled = process.env.WEB_APP_SERVE_ENABLED?.toLowerCase() === 'true';
+if (webAppServeEnabled) {
+    // eslint-disable-next-line no-console
+    console.warn('Building application for web-app-serve');
+}
+const overrideDefine = webAppServeEnabled
+    ? overrideDefineForWebAppServe
+    : undefined;
+
 
 export default defineConfig({
-    // NOTE: We need to replace with URL
-    APP_GRAPHQL_ENDPOINT: Schema.string(),
+    overrideDefine,
+    validator: 'builtin',
+    schema: {
+        // NOTE: These are the dynamic env variables
+        APP_TITLE: Schema.string(),
+        APP_GRAPHQL_ENDPOINT: Schema.string({ format: 'url', protocol: true, tld: false }),
+        APP_GRAPHQL_CODEGEN_ENDPOINT: Schema.string(),
 
-    APP_GRAPHQL_CODEGEN_ENDPOINT:Schema.string(),
-
-    // NOTE: It is not used for now
-    APP_TITLE: Schema.string(),
+    },
 });
